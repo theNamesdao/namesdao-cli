e51d290ad7c9427acdeff12a506c56172b1932fc
Deganawida <deganawida1@protonmail.com>
2022-08-05 22:13:15 -0400

    Remove unnecessary user agent

--- namesdao.py
+++ namesdao.py
@@ -68,9 +68,6 @@ def resolve(name):
         request = Request(
             url,
             data=None,
-            headers={
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
-            },
         )
         response = urlopen(request)
     except HTTPError as err:
